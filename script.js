a51ad2b06e9fa3f39bcfc2a2f7072e592ad9d7ea
shamratPG <shamratpg@gmail.com>
2022-09-15 10:36:16 +0600

    added padding in result tab paragraph

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -81,8 +81,8 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+    <p style="margin: 10px 0;">You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p style="margin: 10px 0;">You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
 
